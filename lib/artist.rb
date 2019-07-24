1b4dc67b4a787797a90bced78ec31cf857452289
Amy Fogelman <abfnj1@gmail.com>
2019-07-24 19:29:50 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  
+ attr_accessor :name
+  
+  def initialize(name)
+     @name = 
+     @songs = []
+  end 
+  
+ def add_song(song)
+   @songs << song
+ end
+
+ def self.all
+  @@all
+ end
+
+ def save
+   self.class.all << self
+  end
+  
+def self.find_or_create_by_name(name)
+ # (my_artist = self.all.find {|artist| artist.name == name}) ? my_artist : self.new(name)
+ self.all.find {|artist| artist.name == name || self.new(name)}
+ end  
+end
+
+ def print_songs
+   self.songs.each do |song|
+     puts song.name
+    end
+  end  
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+class MP3Importer
+  
+  attr_accessor :path
+  
+  def initialize (path)
+    @path = path
+  end
+  
+  def 
+   # files = Dir.glob("#[path)/*.mp3")
+   # files.collect {|file| file.gsub("#{path}/","")}
+
+Dir.globl("#{path}/*.mp3").collect.file.gsub("#{path}/","")}
+
+ end
+ 
+ def Import
+   files.each {|file| Song.new_by_filename}
+end
